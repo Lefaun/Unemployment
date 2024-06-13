98a728f296279e4147e55d24e5ef5ca0eee19b70
Lefaun <118660975+Lefaun@users.noreply.github.com>
2024-06-13 13:29:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -38,10 +38,10 @@ with st.sidebar:
                 print("Não Foi Adicionado CSV")
 
 def filter_data(df: pd.DataFrame) ->pd.DataFrame:
-    options = st.multiselect("escolha a Cena ", options=df.columns)
+    options = st.multiselect("escolha o periodo de desemprego em trimestre ", options=df.columns)
     st.write('Voçê selecionou as seguintes opções', options)
     #adicionei aqui uma cena nova
-    df = pd.read_csv('Mentalhealth3.csv')
+    df = pd.read_csv('Taxa de Desemprego 3.csv')
 def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     """
     Adds a UI on top of a dataframe to let viewers filter columns
